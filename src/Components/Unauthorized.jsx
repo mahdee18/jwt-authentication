1cf03dd897d34a040c795e8d14c68479d017633d
mahdee18 <mahdeec10@gmail.com>
2024-03-18 23:59:39 +0600

    Updated

--- src/Components/Unauthorized.jsx
+++ src/Components/Unauthorized.jsx
@@ -9,7 +9,6 @@ const Unauthorized = () => {
         <section>
             <h1>Unauthorized</h1>
             <br />
-            
             <p>You do not have access to the requested page.</p>
             <div className="flexGrow">
                 <button onClick={goBack}>Go Back</button>
